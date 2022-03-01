5e2978be607be5b46dc227f417913b92205e70d5
Rahul Pandey <rahulpandey862@gmail.com>
2022-03-01 12:10:08 +0530

    Added configuration notebook which includes common configuration used across entire project.

---
+++ notebooks/Formula1/Configuration/configuration.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC This notebook consists of common configuration/path that is used across this complete project.
+
+# COMMAND ----------
+
+raw_folder_path = "abfss://databricksfilecontainer@databricksstaccount12345.dfs.core.windows.net/raw"
+processed_folder_path = "abfss://databricksfilecontainer@databricksstaccount12345.dfs.core.windows.net/processed"
+
+# COMMAND ----------
+
